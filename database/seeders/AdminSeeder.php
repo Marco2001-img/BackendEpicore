4058804f9a8acb5808ae9daed3ff3e5c0b6409b3
Andre Solorzano <andrehorizon25@gmail.com>
2024-10-09 20:51:55 -0600

    seeder admin

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,15 +15,27 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         DB::table('admins')->insert([
-            'name' => 'Administrador de prueba 1',
+            'nombre' => 'Administrador de prueba 1',
+            'telefono' => '1111457890',
             'email' => 'admin@epicore.com',
             'password' => Hash::make('EPICORE1'),
+            'estatus'=> 1,
         ]);
 
         DB::table('admins')->insert([
-            'name' => 'Administrador de prueba 2',
+            'nombre' => 'Administrador de prueba 2',
+            'telefono' => '2221457890',
             'email' => 'admin1@epicore.com',
             'password' => Hash::make('EPICORE2'),
+            'estatus'=> 1,
+        ]);
+
+        DB::table('admins')->insert([
+            'nombre' => 'Administrador de prueba 3',
+            'telefono' => '3331457890',
+            'email' => 'admin2@epicore.com',
+            'password' => Hash::make('EPICORE3'),
+            'estatus'=> 1,
         ]);
     }
 }
